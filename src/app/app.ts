40d3825ec5ea00e98159676d18819b9b933427fc
Abhiscoder <abhishekkumar200m1@gmail.com>
2025-12-27 15:16:22 +0530

    default component (login and signup)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
+  imports: [Login,Signup],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = 'Abhishek';
