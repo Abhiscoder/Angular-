ac605af6b3217ea0159b7245c9be8c46d6873f87
Abhiscoder <abhishekkumar200m1@gmail.com>
2025-12-27 15:23:25 +0530

    customize component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
 import { Signup } from './signup/signup';
+import { profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
-  imports: [Login,Signup],
+  imports: [Login, Signup, profile],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
